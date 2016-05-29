12e4c8d7cf813998cc6a74d7e5637b7928a9d049
Josh Harrison <josh@picklewagon.com>
2016-05-29 12:03:01 -0600

    Initial commit.

---
+++ src/RequestLoggerServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Picklewagon\RequestLogger;
+
+use Illuminate\Support\ServiceProvider;
+
+class RequestLoggerServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
