11cd82d338a35c08da52ec86bbeea4c29e4e55fc
Josh Harrison <josh@picklewagon.com>
2016-05-29 22:53:08 -0600

    Log the request method.

--- src/Middleware/LogRequest.php
+++ src/Middleware/LogRequest.php
@@ -10,6 +10,7 @@ class LogRequest
     public function handle($request, Closure $next)
     {
         Log::info('LogRequest#handle', [
+            'method' => $_SERVER['REQUEST_METHOD'],
             'request' => $request->all(),
         ]);
         return $next($request);
@@ -18,6 +19,7 @@ class LogRequest
     public function terminate($request, $response)
     {
         Log::info('LogRequest#terminate', [
+            'method' => $_SERVER['REQUEST_METHOD'],
             'request' => $request->all(),
             'response' => $response
         ]);
